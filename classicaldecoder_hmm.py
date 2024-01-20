51732a64bdbc28531b9b6911edddf27e00a80b96
unknown <abdimajid.abdiweli@gmail.com>
2024-01-20 05:32:29 +0100

    Redone viterbi, it is broken not sure why I will take a look into it later but the structure looks alright

--- classicaldecoder_hmm.py
+++ classicaldecoder_hmm.py
@@ -15,6 +15,7 @@ class Decoder:
         is_compo_done = False
         counter_to_two = 0
         leng_counter = 0
+        self.states = ["M","I","D"]
         
         with open(hmm, 'r') as hmm_file:
             
@@ -50,9 +51,6 @@ class Decoder:
                 #now we capture the "normal" transition, insertion-emission and emission probabilites
                 #we know the length of the consensus length, the amount of lines after are that amount times 3
                 if is_compo_done and counter_to_two == 0:
-                    print(np.array(line[1:21]).shape[0])
-
-                    print(np.array(line[1:21]))
                     self.emission_matrix = np.vstack((self.emission_matrix,np.array(line[1:21])))
                     counter_to_two += 1
                     continue
@@ -66,50 +64,82 @@ class Decoder:
                     leng_counter += 1
                     if leng_counter == int(self.leng):
                         continue
+        #make all elements into floats, replace all "*" with negative infinity"
+
+
+        self.initial_transition[self.initial_transition.index("*")] = np.inf
+        self.initial_transition = np.array(self.initial_transition, dtype=float)
+        #print(self.initial_transition)
+
+        if "*" in self.initial_insertion_emission:
+            self.initial_insertion_emission[self.initial_insertion_emission.index("*")] = np.inf
+        self.initial_insertion_emission = np.array(self.initial_insertion_emission, dtype=float)
+        #print(self.initial_insertion_emission)
+
+        if "*" in self.emission_matrix:
+            self.emission_matrix[np.where(self.emission_matrix == "*")] = np.inf
+        self.emission_matrix = self.emission_matrix.astype(float)
+        
+        if "*" in self.transition_matrix:
+            self.transition_matrix[np.where(self.transition_matrix == "*")] = np.inf
+        self.transition_matrix = self.transition_matrix.astype(float)
+
+        if "*" in self.insertion_emission_matrix:
+            self.insertion_emission_matrix[np.where(self.insertion_emission_matrix == "*")] = np.inf
+        self.insertion_emission_matrix = self.insertion_emission_matrix.astype(float)
+
+        self.leng = int(self.leng)
+        
 
                     
-            
+      
 
 
-    def viterbi(self):
-        prob_matrix = np.zeros((self.emission_matrix.shape[0],len(self.seq)))
-        state_matrix = np.zeros((self.emission_matrix.shape[0],len(self.seq)))
-        state_list = ["M","I","D","M","I","M","D"]
-        most_seq_states = [None] * len(self.seq)
-
-        #initiate
-        prob_matrix[:,0] = self.initial_insertion_emission[:self.symbols.index(self.seq[0])] * self.initial_transition
-
-        for i in range(1, prob_matrix.shape[1]):
-            for j in range(prob_matrix.shape[0]):
-                temp = prob_matrix[i,j-1] * self.transition_matrix[j,:] #just relized that we need to check what the most probebiable state transition is
-                best_state, = np.argmax(self.transition_matrix[j,:])
-                prob_matrix[i,j] = np.max(temp)*self.emission_matrix[best_state,self.seq[i]]
-                most_seq_states[i-1] = state_list[np.argmax(self.transition_matrix[j,:])]
-
-        print(prob_matrix)
+    def viterbi(self): #rewritten
+        prob_matrix = np.full((3, len(self.seq)), np.inf)
+        backtrack_matrix = np.full((3, len(self.seq)), np.inf)
+        prob_matrix[0,0] = self.initial_transition[0] + self.emission_matrix[0,np.where(self.seq[0])]
+        prob_matrix[1,0] = self.initial_transition[1] + self.initial_insertion_emission[np.where(self.seq[0])]
+        prob_matrix[2,0] = self.initial_transition[2]
 
 
+        #doing what durbin says in chapter 5 for viterbi
+        #might be wrong, but we need to use the initial insertion emission till we score a match state, also transitions such as D > I and vice versa are illegal, so those are inf
+        begin_check=0
+        for i in range(1,prob_matrix.shape[1]):
+            if begin_check == 0:
+              print(prob_matrix)
+              #V(t)M
+              prob_matrix[0,i] = np.min(prob_matrix[:,i-1]+np.vstack([self.transition_matrix[i,0],self.initial_transition[3],self.initial_transition[5]])) + self.emission_matrix[i,np.where(self.seq[i])] #the weird splicing in trans matrix is cuz I messed up earlier
+              backtrack_matrix[0,i] = np.argmin(prob_matrix[:,i-1]+self.transition_matrix[i,[0,3,5]]) 
 
+              #V(t)I
 
+              prob_matrix[1, i] = np.min(prob_matrix[:, i - 1] + (np.vstack([self.transition_matrix[i, 1], self.initial_transition[4], np.inf]))) if np.argmin(prob_matrix[:, i - 1] + (np.vstack([self.transition_matrix[i, 1], self.initial_transition[4], np.inf]))) == 1 + self.emission_matrix[i, np.where(self.seq[i])] else self.initial_insertion_emission[np.where(self.seq[i])]
+              backtrack_matrix[1,i] = np.argmin(prob_matrix[:,i-1]+(np.vstack([self.transition_matrix[i,1],self.initial_transition[4],np.inf])))
 
-    #print(self.transition_matrix)
-    #print(self.insertion_emission_matrix)
-    #print(self.initial_insertion_emission)
-    #print(self.initial_transition)
-    #print(self.nullprob_emission)
+              #V(t)D
+              prob_matrix[2,i] = np.min(prob_matrix[:,i-1]+(np.vstack([self.transition_matrix[i,2],np.inf,np.inf])))
+              backtrack_matrix[2,i] = np.argmin(prob_matrix[:,i-1]+(np.vstack([self.transition_matrix[i,2],np.inf,np.inf])))
                 
+              if np.argmin(prob_matrix[:,i]) == 0:
+                  begin_check += 1
+            if begin_check != 0:
+            #V(t)M
+                prob_matrix[0,i] = np.min(prob_matrix[:,i-1]+self.transition_matrix[i,[0,3,5]]) + self.emission_matrix[i,np.where(self.seq[i])] #the weird splicing in trans matrix is cuz I messed up earlier
+                backtrack_matrix[0,i] = np.argmin(prob_matrix[:,i-1]+self.transition_matrix[i,[0,3,5]]) 
 
+                #V(t)I
+                prob_matrix[1,i] = np.min(prob_matrix[:,i-1]+(np.vstack([self.transition_matrix[i,[1,4]],np.inf]))) + self.emission_matrix[i,np.where(self.seq[i])]
+                backtrack_matrix[1,i] = np.argmin(prob_matrix[:,i-1]+(np.vstack([self.transition_matrix[i,[1,4]],np.inf])))
 
-                
-test = Decoder(["A","A","A","A","A","A","A","A","A","A","A","A","S","G","H","A","G","H","S","K","L"],"globins4.hmm")
-test.viterbi()
+                #V(t)D
+                prob_matrix[2,i] = np.min(prob_matrix[:,i-1]+(np.vstack([self.transition_matrix[i,2],np.inf,self.transition_matrix[i,6]])))
+                backtrack_matrix[2,i] = np.argmin(prob_matrix[:,i-1]+(np.vstack([self.transition_matrix[i,2],np.inf,self.transition_matrix[i,6]])))
+            return prob_matrix
+        
 
-
-
-
-
-
-
+test = Decoder("VLSDAEWQLVLNIWAKVEADVAGHGQDILIRLFKGHPETLEKFDKFKHLKTEAEMKASEDLKKHGNTVLTALGGILKKKGHHEAELKPLAQSHATKHKIIKYLEFISDAIIHVLHSRHPGDFGADAQAAMNKALELFRKDIAAKYKELGFQG".split(),"globins4.hmm")
+lol = test.viterbi()
         
 

815b026972a568a408ec5cc2b38586e0f0916156
unknown <abdimajid.abdiweli@gmail.com>
2024-02-22 18:12:38 +0100

    Decoder now allows viterbi and forward without doublication of code, the code is also redone to allow the implementation of MM state

--- pHMMdecoderV6.py
+++ pHMMdecoderV6.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.special as sp
+import math
 
 import numpy as np
 
@@ -95,12 +96,12 @@ class Decoder:
         self.amm = np.exp(-self.tranM[:,0] )
         self.ami = np.exp(-self.tranM[:,1] )
         self.amd = np.exp(-self.tranM[:,2] )
-        #self.amd[-2] = 0 #This might be used, it is really unclear but some sources states that the Plan7viterbi does NOT use the last delete state, as I understand it, it is enough to remove the first delete state in the core model so each search always matches one residue to match
+        self.amd[-2] = 0 #This might be used, it is really unclear but some sources states that the Plan7viterbi does NOT use the last delete state, as I understand it, it is enough to remove the first delete state in the core model so each search always matches one residue to match
         self.aim = np.exp(-self.tranM[:,3])
         self.aii = np.exp(-self.tranM[:,4])
         self.adm = np.exp(-self.tranM[:,5])
         self.add = np.exp(-self.tranM[:,6]) #decided to seperate them (mm = match to match etc, a for transition as durbin does)
-        #self.add[-2] = 0
+        self.add[-2] = 0
         #print(self.add)
         #print(np.log2(self.amd))
 
@@ -120,7 +121,8 @@ class Decoder:
         #self.nullem0 = [i/100 for i in self.nullem0]
 
 
-    def viterbi(self):
+    def forward(self):
+
         
         for id_seq, stuff in self.sequences:
 
@@ -233,7 +235,226 @@ class Decoder:
             #print(id_seq, (dp_m[-1,-1]["log-odds"] + np.log2(act) - (np.logaddexp2(-len(seq)*np.log2(arr), np.log2(1-arr)))))
             print(id_seq, (dp_m[-1,-1]["log-odds"] + np.log2(act)) - (len(seq)*np.log2(arr) + np.log2(1-arr)))
             #print(dp_m[-1,-1]["log-odds"] - len(seq)*np.log2(act))
+  
 
+
+
+    def inverse(self, alg_base="forward", inverse_mode = False):
+        if alg_base not in ["forward", "viterbi"]:
+            raise ValueError("Error: Not a valid algorithm")
+        if inverse_mode not in [True, False]:
+            raise SyntaxError("Error: Please give a valid boolean value for inverse_mode")
+        elif inverse_mode == True:
+            ir = 0 #inversion ratio, the fraction of transitions that should have gone to M that instead goes to MM
+        else:
+            ir = 0
+        iir = 1-ir #inverted inversion ratio, the fraction that continues to go to M instead of MM
+            
+        
+        
+        for id_seq, stuff in self.sequences:
+                #print("Alignment of", id_seq)
+                seq = list(stuff)
+                
+                self.symb_index = [self.symbols.index(j) for j in seq] #easier usage later
+                
+
+                #now for some transition probabilities, in line with what sean R. EDDY said
+                #negative log transforming it all 
+                anb = act = ajb = 3/(len(seq)+3) #? #pretty much the same as hmmer if 3 is replaced with 6?????? wtf
+                ann = ajj = acc = 1-act
+
+                aej = aec = 0.5
+                abm = 2/((self.leng*(self.leng + 1)))
+                ame = ade = 1
+                nullscore = 0
+                arr = len(seq)/(len(seq)+1) #the transition prob for going from R to R in the null model
+
+
+                #some comments before implementing the MM state
+                #MM state in DP programming requires to look back 2 steps in both row and col, to not get an index error we extand the row (seq) and col (states), these will be nonexistant Seq(-1) state(-1)
+                #MM1 will be seen as the same "level" as M1 and same for MM2 and M2, however the transition probabilities from MM(k) will be the same as for M(k+1)
+                #the transition from other states into MM are the same as into M but multiplied with ir
+
+                dp_m = np.array([[{}]*(self.leng+2+5) for _ in range(len(seq)+2)]) #the last 3 columns are the special states in the order E, J, C and first 2 are N and B, the 2 columns efter B are nonexistant states and the same 
+                #initiating
+                for seqi in range(dp_m.shape[0]):
+                    #print(seqi)
+                    for statej in range(dp_m.shape[1]):
+                        dp_m[seqi,statej] = {"log-odds": -np.inf, "prev": None}
+                #print(dp_m[:,3])
+                for seqi in range(dp_m.shape[0]):
+                    for statej in range(2, dp_m.shape[1]-3):
+                        dp_m[seqi,statej] = {"log-odds M": -np.inf,"log-odds I": -np.inf,"log-odds D": -np.inf, "prev": None}
+                
+                #Start is from dp[0,N] = 0 (nat log transformed so prob 1)
+                dp_m[1,0] = {"log-odds": 0, "prev": None}
+                #dp[0,B] = the transistion from N to B
+                dp_m[1,1] = {"log-odds": np.log2(anb), "prev": ("N", [0,0])}
+                #print(dp_m[0,3]["log-odds M"])
+                for seqi in range(2, dp_m.shape[0]):
+                    #print(self.symbols[self.symb_index[seqi-1]])
+                    em_rowind = 0
+                    inem_rowin = 1
+                    for statej in range(4, dp_m.shape[1]-3):
+                        
+                        #because of how MM state cant exist when there is only one residue left OR when we are evaluating the last state
+                        if seqi == dp_m.shape[0]-1 or statej == dp_m.shape[1]-4:
+                            ir = 0
+                            irr = 1-0
+                        else:
+                            ir = 0
+                            irr = 1-ir
+
+                        #m
+                        T = np.log2(np.exp2(dp_m[seqi-1,statej-1]["log-odds M"]) * self.amm[em_rowind] * iir)
+                        Q = np.log2(np.exp2(dp_m[seqi-1,statej-1]["log-odds I"]) * self.aim[em_rowind] * iir)
+                        S = np.log2(np.exp2(dp_m[seqi-1,statej-1]["log-odds D"]) * self.adm[em_rowind] * iir)
+                        P = np.log2(np.exp2(dp_m[seqi-1,1]["log-odds"]) * abm * iir)
+                        if alg_base == "forward":
+                            log_tot = np.logaddexp2.reduce([P,Q,T,S])
+                        elif alg_base == "viterbi":
+                            log_tot, prev_ind = np.max([T,Q,S,P]), np.argmax([T,Q,S,P])
+                            
+                            prev_state = ["M","I","D","B"][prev_ind]
+                            #print(prev_state)
+                            prev_pos = [[seqi-1,statej-1], [seqi-1,statej-1], [seqi-1,statej-1], [seqi-1,1]][prev_ind]
+                            dp_m[seqi,statej]["prev M"] = (prev_state, prev_pos)
+                            #print(dp_m[seqi,statej]["prev"])
+                        dp_m[seqi,statej]["log-odds M"] = log_tot + np.log2((self.emM[em_rowind, self.symb_index[seqi-2]]/self.nullem0[self.symb_index[seqi-2]]))
+
+
+
+                        #i
+                        P = np.log2(np.exp2(dp_m[seqi-1,statej]["log-odds M"]) * self.ami[em_rowind])
+                        Q = np.log2(np.exp2(dp_m[seqi-1,statej]["log-odds I"]) * self.aii[em_rowind])
+                        #temp = np.exp2(dp_m[seqi-1,statej]["log-odds M"]) * self.ami[em_rowind-1] + np.exp2(dp_m[seqi-1,statej]["log-odds I"]) * self.aii[em_rowind-1]
+                        if alg_base == "forward":    
+                            PQ = np.logaddexp2(P,Q)
+                        elif alg_base == "viterbi":
+                            PQ, prev_ind = np.max([P,Q]), np.argmax([P,Q])
+                            prev_state = ["M","I"][prev_ind]
+                            prev_pos = [[seqi-1,statej], [seqi-1,statej]][prev_ind]
+                            dp_m[seqi,statej]["prev I"] = (prev_state, prev_pos)
+
+                        #print(PQ)
+                        dp_m[seqi,statej]["log-odds I"] = PQ + np.log2((self.inemM[inem_rowin, self.symb_index[seqi-2]]/self.nullem0[self.symb_index[seqi-2]]))
+
+                        #d
+                        P = np.log2(np.exp2(dp_m[seqi,statej-1]["log-odds M"]) * self.amd[em_rowind])
+                        Q = np.log2(np.exp2(dp_m[seqi,statej-1]["log-odds D"]) * self.add[em_rowind])
+                        if alg_base == "forward":    
+                            PQ = np.logaddexp2(P,Q)
+                        elif alg_base == "viterbi":
+                            PQ, prev_ind = np.max([P,Q]), np.argmax([P,Q])
+                            prev_state = ["M","D"][prev_ind]
+                            prev_pos = [[seqi,statej-1], [seqi,statej-1]][prev_ind]
+                            dp_m[seqi,statej]["prev D"] = (prev_state, prev_pos)
+                        dp_m[seqi,statej]["log-odds D"] = PQ 
+
+
+
+                        em_rowind += 1
+                        inem_rowin += 1
+                        
+                    #e
+                    holdit = []
+                    state_remeber = []
+                    stateind_remember = []
+                    for statej in range(4, dp_m.shape[1]-3):
+                        holdit.append(np.log(np.exp2(dp_m[seqi,statej]["log-odds M"]) * ame))
+                        holdit.append(np.log(np.exp2(dp_m[seqi,statej]["log-odds D"]) * ade))
+                        state_remeber += ["M", "D"]
+                        stateind_remember += [statej, statej]
+                    if alg_base == "forward":
+                        log_tot = sp.logsumexp(holdit)
+                    elif alg_base == "viterbi":
+                        log_tot, prev_ind = np.max(holdit), np.argmax(holdit)
+                        prev_state = state_remeber[prev_ind]
+                        prev_pos = stateind_remember[prev_ind]
+                        #print(prev_pos)
+                        #print(seqi)
+                        #prev_pos = np.where(dp_m["log-odds M"] == log_tot)
+                        #print(prev_pos)
+                        #prev_pos = [index.tolist() for index in prev_pos]
+                        dp_m[seqi,-3]["prev"] = (prev_state, [seqi,prev_pos])
+                    log_tot = log_tot/np.log(2)
+                    #log_tot = np.logaddexp2.reduce(holdit)
+                    #print(log_tot)
+
+                    dp_m[seqi,-3]["log-odds"] = log_tot
+
+                    #n
+                    dp_m[seqi,0]["log-odds"] = dp_m[seqi-1,0]["log-odds"] + np.log2(ann) #np.log2(np.exp2(dp_m[seqi-1,0]["log-odds"]) * ann)
+                    dp_m[seqi,0]["prev"] = ("N", [seqi-1,0])
+
+                    #j
+                    P = np.log2(np.exp2(dp_m[seqi,-3]["log-odds"]) * aej)
+                    Q = np.log2(np.exp2(dp_m[seqi-1,-2]["log-odds"]) * ajj)
+                    if alg_base == "forward":
+                        PQ = np.logaddexp2(P,Q)
+                    elif alg_base == "viterbi":
+                        PQ, prev_ind = np.max([P,Q]), np.argmax([P,Q])
+                        prev_state = ["E", "J"][prev_ind]
+                        prev_pos = [[seqi,-3], [seqi-1,-2]][prev_ind]
+                        dp_m[seqi,-2]["prev"] = (prev_state, prev_pos)
+                    dp_m[seqi,-2]["log-odds"] = PQ
+                    
+                    #c
+                    P = np.log2(np.exp2(dp_m[seqi,-3]["log-odds"]) * aec)
+                    Q = np.log2(np.exp2(dp_m[seqi-1,-1]["log-odds"]) * acc)
+                    if alg_base == "forward":
+                        PQ = np.logaddexp2(P,Q)
+                    elif alg_base == "viterbi":
+                        PQ, prev_ind = np.max([P,Q]), np.argmax([P,Q])
+                        prev_state = ["E", "C"][prev_ind]
+                        prev_pos = [[seqi,-3], [seqi-1,-1]][prev_ind]
+                        dp_m[seqi,-1]["prev"] = (prev_state, prev_pos)
+
+                    dp_m[seqi,-1]["log-odds"] = PQ
+
+                    #b
+                    P = np.log2(np.exp2(dp_m[seqi,0]["log-odds"]) * anb)
+                    Q = np.log2(np.exp2(dp_m[seqi,-2]["log-odds"]) * ajb)
+                    if alg_base == "forward":
+                        PQ = np.logaddexp2(P,Q)
+                    elif alg_base == "viterbi":
+                        PQ, prev_ind = np.max([P,Q]), np.argmax([P,Q])
+                        prev_state = ["N", "J"][prev_ind]
+                        prev_pos = [[seqi,0], [seqi,-2]][prev_ind]
+                        dp_m[seqi,1]["prev"] = (prev_state, prev_pos)
+
+                    dp_m[seqi,1]["log-odds"] = PQ
+
+                #print(id_seq, (dp_m[-1,-1]["log-odds"] + np.log2(act) - (np.logaddexp2(-len(seq)*np.log2(arr), np.log2(1-arr)))))
+                print(id_seq, (dp_m[-1,-1]["log-odds"] + np.log2(act)) - (len(seq)*np.log2(arr) + np.log2(1-arr)))
+                #print(dp_m[:,3])
+                #print(dp_m[-1,-1]["log-odds"] - len(seq)*np.log2(act))
+                if alg_base == "viterbi":
+                    path = []
+                    #print(dp_m[50,104]["prev"])
+                    path.append(dp_m[-1,-1]["prev"])
+                    for _ in range(len(seq)+3):
+                        #print(path[-1])
+                        #print(path[-1][1])
+                        #print([path[-1][1][0], path[-1][1][1]])
+                        if path[-1][0] in ["M","I", "D"]:
+                            if path[-1][0] == "M":
+                                path.append(dp_m[path[-1][1][0], path[-1][1][1]]["prev M"])
+                            elif path[-1][0] == "I":
+                                path.append(dp_m[path[-1][1][0], path[-1][1][1]]["prev I"])
+                            elif path[-1][0] == "D":
+                                path.append(dp_m[path[-1][1][0], path[-1][1][1]]["prev D"])
+                        else:
+                            path.append(dp_m[path[-1][1][0], path[-1][1][1]]["prev"])
+                    capital_letters_only = [item[0] for item in path if item[0].isalpha() and item[0].isupper()]
+                    capital_letters_only.reverse()
+                    capital_letters_only = ''.join(capital_letters_only)
+                    print(capital_letters_only)
+
+
+
+                
             
 
 
@@ -243,5 +464,6 @@ class Decoder:
                 
 
                 
-test = Decoder("Testse.txt","globins4.hmm")
-test.viterbi()
+test = Decoder("globins45.fa","globins4.hmm")
+#test.forward()
+test.inverse(alg_base="forward", inverse_mode = False)
